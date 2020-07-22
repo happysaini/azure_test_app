c29116ba97e55db73f6b11d43057bc97f6aa37c8
sweet.lady866@gmail.com <sweet.lady866@gmail.com>
2020-07-22 22:02:55 +0530

    added slack

--- script/slack_notifications.py
+++ script/slack_notifications.py
@@ -1,71 +1,32 @@
 import requests
-import html2text
-from bs4 import BeautifulSoup
 import argparse
 
-webhook_url = "https://hooks.slack.com/services/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"
-payload = { 'channels': ['#cicd-test'] }
+webhook_url = "https://hooks.slack.com/services/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"
+payload = {'channels': ['#ci-cd']}
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--html_report_path', type=str, required=False, default=None, help="Path to the HTML report")
-    parser.add_argument('--build_number', type=str, required=False, default=None, help="CodeBuild Build Number")
+    parser.add_argument('--host_address', type=str, required=False, default=None, help="Host machine address")
     arguments = parser.parse_args()
 
-    with open(arguments.html_report_path, "r") as f:
-        text = f.read()
-        soup = BeautifulSoup(text, 'html.parser')
-        status = soup.find_all('span', attrs={'class': 'method-stats'})
+    attachments = [
+      {
+          "color": "#2eb886",
+          "pretext": "AWS CodePipeline has completed successfully.",
+          "title": "Library Management System",
+          "title_link": "http://{}:8000/".format(arguments.host_address),
+          "text": " The web application has been deployed over AWS EC2 Instance",
+          "fields": [
+              {
+                  "title": "Application Server URL",
+                  "value": "http://{}:8000/".format(arguments.host_address)
+              }
+          ],
+          "footer": "Copper App notification"
 
-        h = html2text.HTML2Text()
-        status = h.handle(str(status[0]))
-    
-    
-    methods = "0 method"
-    passed = "0 passed"
-    failed = "0 failed"
-    skipped= "0 skipped"
-    get_status = status.split(",")
-    for i in get_status:
-        if "methods" in i:
-            methods = i
-        elif "passed" in i:
-            passed = i
-        elif "failed" in i:
-            failed = i
-        elif "skip" in i:
-            skipped = i
-    
-    title = "Mobile CI/CD:  Tests execution details for build #" + str(arguments.build_number)
-    
-    if(failed != "0 failed"):
-        status = "danger"
-    elif(skipped != "0 skipped"):
-        if(failed != "0 failed"):
-            status = "danger"
-        else:
-            status = "warning"
-    else:
-        status = "good"
-
-    attachments = [{
-        "pretext": title,
-        "color": status,
-        "fields": [{
-            "title": "Passed Test Cases",
-            "value": passed,
-            "short": True,
-        }, {
-            "title": "Failed Test Cases",
-            "value": failed,
-            "short": True
-        }, {
-            "title": "Skipped Test Cases",
-            "value": skipped,
-            "short": True
-        }]
-    }]
+      }
+  ]
     payload['attachments'] = attachments
     r = requests.post(webhook_url, json=payload)
     print(r)
